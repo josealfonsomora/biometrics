bc469b697dda3e76b75f67e120bc4802462806c5
Jose Alfonso Mora <josealfonsomora@gmail.com>
2018-11-29 16:54:31 +0100

    FingerprintManager

--- app/src/main/java/com/example/josealfonsomora/biometricsp/FingerprintActivity.kt
+++ app/src/main/java/com/example/josealfonsomora/biometricsp/FingerprintActivity.kt
@@ -1,76 +1,42 @@
 package com.example.josealfonsomora.biometricsp
 
-import android.content.DialogInterface
-import android.graphics.Color
-import android.hardware.biometrics.BiometricPrompt
+import android.annotation.SuppressLint
 import android.hardware.fingerprint.FingerprintManager
-import android.os.Build
 import android.os.Bundle
 import android.os.CancellationSignal
-import android.support.annotation.RequiresApi
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_fingerprint.*
 
+@SuppressLint("SetTextI18n")
 class FingerprintActivity : AppCompatActivity() {
 
-    @RequiresApi(Build.VERSION_CODES.P)
-    private val biometricPromptCallback =
-        object : BiometricPrompt.AuthenticationCallback() {
-            override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
-                super.onAuthenticationError(errorCode, errString)
-                fingerPrintCallback.text = "$errorCode : $errString"
-            }
+    val cancellationSignal = CancellationSignal()
 
-            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
-                super.onAuthenticationSucceeded(result)
-                fingerPrintCallback.text = "onAuthenticationSucceeded"
-            }
-
-            override fun onAuthenticationHelp(helpCode: Int, helpString: CharSequence?) {
-                super.onAuthenticationHelp(helpCode, helpString)
-                fingerPrintCallback.text = "$helpCode : $helpString"
-            }
-
-            override fun onAuthenticationFailed() {
-                super.onAuthenticationFailed()
-                fingerPrintCallback.text = "onAuthenticationFailed"
-            }
-        }
-
-    private val authCallback = object : FingerprintManager.AuthenticationCallback() {
+    val callback = object : FingerprintManager.AuthenticationCallback() {
         override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
             super.onAuthenticationError(errorCode, errString)
-            fingerPrintCallback.text = "$errorCode : $errString"
+            fingerPrintCallback.text = "Error Code: $errorCode = $errString"
         }
 
         override fun onAuthenticationSucceeded(result: FingerprintManager.AuthenticationResult?) {
             super.onAuthenticationSucceeded(result)
             fingerPrintCallback.text = "onAuthenticationSucceeded"
-
         }
 
         override fun onAuthenticationHelp(helpCode: Int, helpString: CharSequence?) {
             super.onAuthenticationHelp(helpCode, helpString)
-            fingerPrintCallback.text = "$helpCode : $helpString"
-
+            fingerPrintCallback.text = "Help Code: $helpCode = $helpString"
         }
 
         override fun onAuthenticationFailed() {
             super.onAuthenticationFailed()
-            fingerPrintCallback.text = "onAuthenticationFailed"
-        }
-    }
-
-    private val cancelSignal = CancellationSignal().apply {
-        setOnCancelListener {
-            fingerPrintCallback.text = "setOnCancelListener"
+            fingerPrintCallback.text = "Authentication failed"
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_fingerprint)
     }
 
@@ -79,55 +45,25 @@ class FingerprintActivity : AppCompatActivity() {
 
         val fingerprintManager = getSystemService(FingerprintManager::class.java)
 
-        hardwareDetectedButton.setBackgroundColor(
-            if (fingerprintManager.isHardwareDetected) {
-                Color.GREEN
-            } else {
-                Color.RED
-            }
-        )
+        fingerprintManager.isHardwareDetected
 
-        fingerPrintSaved.setBackgroundColor(
-            if (fingerprintManager.hasEnrolledFingerprints()) {
-                Color.GREEN
-            } else {
-                Color.RED
-            }
-        )
+        fingerprintManager.hasEnrolledFingerprints()
+
+        cancelFingerprint.setOnClickListener {
+            cancellationSignal.cancel()
+        }
 
         fingerprint.setOnClickListener {
+            fingerPrintCallback.text = "Listening.."
             cancelFingerprint.visibility = View.VISIBLE
-            cancelFingerprint.setOnClickListener { cancelSignal.cancel() }
 
-            if (fingerprintManager.isHardwareDetected && fingerprintManager.hasEnrolledFingerprints()) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                    openBiometricPrompt().authenticate(
-                        cancelSignal,
-                        mainExecutor,
-                        biometricPromptCallback
-                    )
-                } else {
-                    fingerprintManager.authenticate(
-                        null,
-                        cancelSignal,
-                        0,
-                        authCallback,
-                        null
-                    )
-                }
-            }
+            fingerprintManager.authenticate(
+                null, // cryptoObject
+                cancellationSignal, // cancel listener
+                0, // should be 0
+                callback, // FingerprintManager.AuthenticationCallback()
+                null // Handler to run callbacks
+            )
         }
     }
-
-
-    @RequiresApi(Build.VERSION_CODES.P)
-    private fun openBiometricPrompt() =
-        BiometricPrompt.Builder(this)
-            .setDescription("description")
-            .setNegativeButton("no", mainExecutor,
-                DialogInterface.OnClickListener { dialog, which -> fingerPrintCallback.text = "Negative clicked" })
-            .setSubtitle("tubtitle")
-            .setTitle("title")
-            .build()
-
 }
